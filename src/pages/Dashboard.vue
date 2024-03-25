d829eb3000df593c2952e59d7192474eeebb1040
Shazrin98 <shafiqnazrin5@gmail.com>
2024-03-25 13:06:31 +0800

    finishied creating all APIs for the dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -69,7 +69,7 @@
             </h5>
             <h3 class="card-title">
               <i class="tim-icons icon-delivery-fast text-success"></i>
-              {{ droppedTraffic }}
+              {{ formatNumber(droppedTraffic) }}
             </h3>
           </template>
         </card>
@@ -103,8 +103,8 @@
               {{ $t("dashboard.droppedTrafficSeverityInternet") }}
             </h5>
             <ul class="list-group list-group-flush">
-              <li v-for="severityData in droppedTrafficSeverityInternet" :key="severityData.severity">
-                {{ severityData.severity }}: {{ severityData.count }}
+              <li v-for="severityData in droppedTrafficSeverityInternet" :key="severityData.key">
+                {{ severityData.key }}: {{ formatNumber(severityData.docCount) }}
               </li>
             </ul>
           </template>
@@ -123,8 +123,8 @@
               {{ $t("dashboard.top5CountryTrafficAllowed") }}
             </h5>
             <ul class="list-group list-group-flush">
-              <li v-for="countryData in top5CountryTrafficAllowed" :key="countryData.severity">
-                {{ countryData.country }}: {{ countryData.count }}
+              <li v-for="countryData in top5CountryTrafficAllowed" :key="countryData.key">
+                {{ countryData.key }}: {{ formatNumber(countryData.docCount) }}
               </li>
             </ul>
           </template>
@@ -139,8 +139,8 @@
               {{ $t("dashboard.top5CountryTrafficBlocked") }}
             </h5>
             <ul class="list-group list-group-flush">
-              <li v-for="countryData in top5CountryTrafficBlocked" :key="countryData.severity">
-                {{ countryData.country }}: {{ countryData.count }}
+              <li v-for="countryData in top5CountryTrafficBlocked" :key="countryData.key">
+                {{ countryData.key }}: {{ formatNumber(countryData.docCount) }}
               </li>
             </ul>
           </template>
@@ -158,8 +158,8 @@
             {{ $t("dashboard.vpnUsersConnected") }}
           </h5>
           <ul class="list-group list-group-flush list-unstyled">
-            <li v-for="(user, index) in vpnUsersConnected" :key="index">
-              <i class="tim-icons icon-single-02 text-success"></i> {{ user }}
+            <li v-for="vpnUserData in vpnUsersConnected" :key="vpnUserData.key">
+              <i class="tim-icons icon-single-02 text-success"></i> {{ vpnUserData.key }}: {{ formatNumber(vpnUserData.docCount) }}
             </li>
           </ul>
         </card>
@@ -178,7 +178,7 @@
             </h5>
             <h3 class="card-title">
               <i class="tim-icons icon-email-85 text-success"></i>
-              {{ successfulReceivedEmail }}
+              {{ formatNumber(successfulReceivedEmail) }}
             </h3>
           </template>
         </card>
@@ -193,7 +193,7 @@
             </h5>
             <h3 class="card-title">
               <i class="tim-icons icon-email-85 text-success"></i>
-              {{ quarantinedReceivedEmail }}
+              {{ formatNumber(quarantinedReceivedEmail) }}
             </h3>
           </template>
         </card>
@@ -208,7 +208,7 @@
             </h5>
             <h3 class="card-title">
               <i class="tim-icons icon-email-85 text-success"></i>
-              {{ failedReceivedEmail }}
+              {{ formatNumber(failedReceivedEmail) }}
             </h3>
           </template>
         </card>
@@ -226,8 +226,8 @@
               {{ $t("dashboard.top10AppsUsedInternally") }}
             </h5>
             <ul class="list-group list-group-flush">
-              <li v-for="appData in top10AppsUsedInternally" :key="appData.app">
-                {{ appData.app }}: {{ appData.count }}
+              <li v-for="appData in top10AppsUsedInternally" :key="appData.key">
+                {{ appData.key }}: {{ formatNumber(appData.docCount) }}
               </li>
             </ul>
           </template>
@@ -242,8 +242,8 @@
               {{ $t("dashboard.top10RequestedAppsGovNet") }}
             </h5>
             <ul class="list-group list-group-flush">
-              <li v-for="domainData in top10RequestedAppsGovNet" :key="domainData.domain">
-                {{ domainData.domain }}: {{ domainData.count }}
+              <li v-for="appData in top10RequestedAppsGovNet" :key="appData.key">
+                {{ appData.key }}: {{ formatNumber(appData.docCount) }}
               </li>
             </ul>
           </template>
@@ -258,8 +258,8 @@
               {{ $t("dashboard.top10RequestedAppsInternet") }}
             </h5>
             <ul class="list-group list-group-flush">
-              <li v-for="domainData in top10RequestedAppsInternet" :key="domainData.domain">
-                {{ domainData.domain }}: {{ domainData.count }}
+              <li v-for="appData in top10RequestedAppsInternet" :key="appData.key">
+                {{ appData.key }}: {{ formatNumber(appData.docCount) }}
               </li>
             </ul>
           </template>
@@ -279,20 +279,30 @@ import * as chartConfigs from "@/components/Charts/config";
 import TaskList from "./Dashboard/TaskList";
 import UserTable from "./Dashboard/UserTable";
 import config from "@/config";
-import { MotCounts } from "@/data/mockData.js";
 import { getAllowedTraffic } from '@/services/api.service';
+import { getDroppedTraffic } from '@/services/api.service';
 import { getDroppedTrafficSeverityGovNet } from '@/services/api.service';
+import { getDroppedTrafficSeverityInternet } from '@/services/api.service';
+import { getTop5CountryTrafficAllowed } from '@/services/api.service';
+import { getTop5CountryTrafficBlocked } from '@/services/api.service';
+import { getVpnUsersConnected } from '@/services/api.service';
+import { getSuccessfulReceivedEmail } from '@/services/api.service';
+import { getQuarantinedReceivedEmail } from '@/services/api.service';
+import { getFailedReceivedEmail } from '@/services/api.service';
+import { getTop10AppsUsedInternally } from '@/services/api.service';
+import { getTop10RequestedAppsGovNet } from '@/services/api.service';
+import { getTop10RequestedAppsInternet } from '@/services/api.service';
 
 export default {
   components: {
     LineChart,
-    BarChart
+    BarChart,
   },
   data() {
     return {
-      allowedTraffic: 0, /////////////////////////////////
+      allowedTraffic: 0,
       droppedTraffic: 0,
-      droppedTrafficSeverityGovNet: 0,////////////////////
+      droppedTrafficSeverityGovNet: 0,
       droppedTrafficSeverityInternet: 0,
       top5CountryTrafficAllowed: 0,
       top5CountryTrafficBlocked: 0,
@@ -451,20 +461,105 @@ export default {
         console.log('Dashboard fetchData allowedTrafficResponse:', allowedTrafficResponse);//////////////////
         this.allowedTraffic = allowedTrafficResponse && allowedTrafficResponse.count ? allowedTrafficResponse.count : 0;
 
+        // Dropped Traffic
+        const droppedTrafficResponse = await getDroppedTraffic(timeRange);
+        console.log('Dashboard fetchData droppedTrafficResponse:', droppedTrafficResponse);//////////////////
+        this.droppedTraffic = droppedTrafficResponse && droppedTrafficResponse.count ? droppedTrafficResponse.count : 0;
+
+        // Successful Received Email
+        const successfulReceivedEmailResponse = await getSuccessfulReceivedEmail(timeRange);
+        console.log('Dashboard fetchData successfulReceivedEmailResponse:', successfulReceivedEmailResponse);//////////////////
+        this.successfulReceivedEmail = successfulReceivedEmailResponse && successfulReceivedEmailResponse.count ? successfulReceivedEmailResponse.count : 0;
+
+        // Quarantined Received Email
+        const quarantinedReceivedEmailResponse = await getQuarantinedReceivedEmail(timeRange);
+        console.log('Dashboard fetchData quarantinedReceivedEmailResponse:', quarantinedReceivedEmailResponse);//////////////////
+        this.quarantinedReceivedEmail = quarantinedReceivedEmailResponse && quarantinedReceivedEmailResponse.count ? quarantinedReceivedEmailResponse.count : 0;
+
+        // Failed Received Email
+        const failedReceivedEmailResponse = await getFailedReceivedEmail(timeRange);
+        console.log('Dashboard fetchData failedReceivedEmailResponse:', failedReceivedEmailResponse);//////////////////
+        this.failedReceivedEmail = failedReceivedEmailResponse && failedReceivedEmailResponse.count ? failedReceivedEmailResponse.count : 0;
+
         // Dropped Traffic Severity By GovNet Source
         const droppedTrafficSeverityGovNetResponse = await getDroppedTrafficSeverityGovNet(timeRange);
         console.log('Dashboard fetchData droppedTrafficSeverityGovNetResponse:', droppedTrafficSeverityGovNetResponse);
-        // Extract relevant data from the dropped traffic severity response
-        const buckets = droppedTrafficSeverityGovNetResponse.aggregations.top_attacks.buckets;
-        // Map the extracted data to get an array of objects with keys and doc counts
-        const droppedTrafficSeverityData = buckets.map(bucket => ({
+        const DTSGNbuckets = droppedTrafficSeverityGovNetResponse.aggregations.top_attacks.buckets;
+        const droppedTrafficSeverityGovNetData = DTSGNbuckets.map(bucket => ({
           key: bucket.key,
           docCount: bucket.doc_count,
         }));
-        // Update droppedTrafficSeverityGovNet with the new data
-        this.droppedTrafficSeverityGovNet = droppedTrafficSeverityData;
+        this.droppedTrafficSeverityGovNet = droppedTrafficSeverityGovNetData;
 
+        // Dropped Traffic Severity By Internet Source////////
+        const droppedTrafficSeverityInternetResponse = await getDroppedTrafficSeverityInternet(timeRange);
+        console.log('Dashboard fetchData droppedTrafficSeverityInternetResponse:', droppedTrafficSeverityInternetResponse);
+        const DTSIbuckets = droppedTrafficSeverityInternetResponse.aggregations.top_attacks.buckets;
+        const droppedTrafficSeverityInternetData = DTSIbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.droppedTrafficSeverityInternet = droppedTrafficSeverityInternetData;
 
+        // Top 5 Country Traffic Allowed////////////
+        const top5CountryTrafficAllowedResponse = await getTop5CountryTrafficAllowed(timeRange);
+        console.log('Dashboard fetchData top5CountryTrafficAllowedResponse:', top5CountryTrafficAllowedResponse);
+        const T5CTAbuckets = top5CountryTrafficAllowedResponse.aggregations.top_countries.buckets;
+        const top5CountryTrafficAllowedData = T5CTAbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.top5CountryTrafficAllowed = top5CountryTrafficAllowedData;
+
+        // Top 5 Country Traffic Blocked//////////
+        const top5CountryTrafficBlockedResponse = await getTop5CountryTrafficBlocked(timeRange);
+        console.log('Dashboard fetchData top5CountryTrafficBlockedResponse:', top5CountryTrafficBlockedResponse);
+        const T5CTBbuckets = top5CountryTrafficBlockedResponse.aggregations.top_countries.buckets;
+        const top5CountryTrafficBlockedData = T5CTBbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.top5CountryTrafficBlocked = top5CountryTrafficBlockedData;
+
+        // VPN Users Connected/////////
+        const vpnUsersConnectedResponse = await getVpnUsersConnected(timeRange);
+        console.log('Dashboard fetchData vpnUsersConnectedResponse:', vpnUsersConnectedResponse);
+        const VUCbuckets = vpnUsersConnectedResponse.aggregations.top_users.buckets;
+        const vpnUsersConnectedData = VUCbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.vpnUsersConnected = vpnUsersConnectedData;
+
+        // Top 10 Apps Used Internally//////////
+        const top10AppsUsedInternallyResponse = await getTop10AppsUsedInternally(timeRange);
+        console.log('Dashboard fetchData top10AppsUsedInternallyResponse:', top10AppsUsedInternallyResponse);
+        const T10AUIbuckets = top10AppsUsedInternallyResponse.aggregations.top_websites.buckets;
+        const top10AppsUsedInternallyData = T10AUIbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.top10AppsUsedInternally = top10AppsUsedInternallyData;
+
+        // Top 10 Requested Apps By GovNet//////////
+        const top10RequestedAppsGovNetResponse = await getTop10RequestedAppsGovNet(timeRange);
+        console.log('Dashboard fetchData top10RequestedAppsGovNetResponse:', top10RequestedAppsGovNetResponse);
+        const T10RAGNbuckets = top10RequestedAppsGovNetResponse.aggregations.top_websites.buckets;
+        const top10RequestedAppsGovNetData = T10RAGNbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.top10RequestedAppsGovNet = top10RequestedAppsGovNetData;
+
+        // Top 10 Requested Apps By Internet//////
+        const top10RequestedAppsInternetResponse = await getTop10RequestedAppsInternet(timeRange);
+        console.log('Dashboard fetchData top10RequestedAppsInternetResponse:', top10RequestedAppsInternetResponse);
+        const T10RAIbuckets = top10RequestedAppsInternetResponse.aggregations.top_website.buckets;
+        const top10RequestedAppsInternetData = T10RAIbuckets.map(bucket => ({
+          key: bucket.key,
+          docCount: bucket.doc_count,
+        }));
+        this.top10RequestedAppsInternet = top10RequestedAppsInternetData;        
 
       } catch (error) {
         console.error('Error fetching allowed traffic:', error);
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -11,67 +11,379 @@ const api = axios.create({
   },
 });
 
-/////////////////////////////////////////////////////////////////////////
-  export const getAllowedTraffic = async (timeRange = {}) => {
-    const url = "/raven*/_count";
-    const data = {
-      query: {
-        bool: {
-          must: [
-            { wildcard: { "data.action": "pass" } },
-            { wildcard: { "decoder.name": "fortigate-firewall-v5" } },
-            { range: { "@timestamp": timeRange } },
-          ],
-        },
-      },
-    };
-
-    console.log("getAllowedTraffic data:", data);/////////////
-    
-    try {
-        const response = await api.post(url, data);
-        return response.data;
-      } catch (error) {
-        console.error("Error fetching allowed traffic:", error);
-        throw new Error(`Failed to fetch allowed traffic data: ${error.message}`);
-      }
-};
-/////////////////////////////////////////////////////////////////////////
-export const getDroppedTrafficSeverityGovNet = async (timeRange = {}) => {
-  const url = "/raven*/_search?pretty";
+// allowedTraffic
+export const getAllowedTraffic = async (timeRange = {}) => {
+  const url = "/raven*/_count";
   const data = {
-    "size": 0,
-    "query": {
-      "bool": {
-        "must": [
-          { "prefix": { "data.srcip": "10." } },
-          { "range": { "@timestamp": timeRange } }
-        ]
-      }
+    query: {
+      bool: {
+        must: [
+          { wildcard: { "data.action": "pass" } },
+          { wildcard: { "decoder.name": "fortigate-firewall-v5" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
     },
-    "aggs": {
-      "top_attacks": {
-        "terms": {
-          "field": "data.apprisk",
-          "size": 5
-        }
-      }
-    }
   };
-
-  console.log("getDroppedTrafficSeverityGovNet data:", data);////////
-
   try {
     const response = await api.post(url, data);
     return response.data;
   } catch (error) {
-    console.error("Error fetching dropped traffic severity:", error);
-    throw new Error(`Failed to fetch dropped traffic severity data: ${error.message}`);
+    console.error("Error fetching allowedTraffic:", error);
+    throw new Error(`Failed to fetch allowedTraffic data: ${error.message}`);
+  }
+};
+// droppedTraffic
+export const getDroppedTraffic = async (timeRange = {}) => {
+  const url = "/raven*/_count";
+  const data = {
+    query: {
+      bool: {
+        must: [
+          { wildcard: { "data.action": "dropped" } },
+          { wildcard: { "decoder.name": "fortigate-firewall-v5" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching droppedTraffic:", error);
+    throw new Error(`Failed to fetch droppedTraffic data: ${error.message}`);
+  }
+};
+// successfulReceivedEmail
+export const getSuccessfulReceivedEmail = async (timeRange = {}) => {
+  const url = "/raven*/_count";
+  const data = {
+    query: {
+      bool: {
+        must: [
+          { match: { "data.finalaction": "DELIVERED" } },
+          { match: { "decoder.name": "Email_security" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching successfulReceivedEmail:", error);
+    throw new Error(
+      `Failed to fetch successfulReceivedEmail data: ${error.message}`
+    );
+  }
+};
+// quarantinedReceivedEmail
+export const getQuarantinedReceivedEmail = async (timeRange = {}) => {
+  const url = "/raven*/_count";
+  const data = {
+    query: {
+      bool: {
+        must: [
+          { match: { "data.finalaction": "QUARANTINED" } },
+          { match: { "decoder.name": "Email_security" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching quarantinedReceivedEmail:", error);
+    throw new Error(
+      `Failed to fetch quarantinedReceivedEmail data: ${error.message}`
+    );
+  }
+};
+// failedReceivedEmail/////////
+export const getFailedReceivedEmail = async (timeRange = {}) => {
+  const url = "/raven*/_count";
+  const data = {
+    query: {
+      bool: {
+        must: [
+          { match: { "data.finalaction": "ABORTED" } },
+          { match: { "decoder.name": "Email_security" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching failedReceivedEmail:", error);
+    throw new Error(
+      `Failed to fetch failedReceivedEmail data: ${error.message}`
+    );
+  }
+};
+/////////////////////////////////////////////////////////////////////////
+// droppedTrafficSeverityGovNet
+export const getDroppedTrafficSeverityGovNet = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          { prefix: { "data.srcip": "10." } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+    aggs: {
+      top_attacks: {
+        terms: {
+          field: "data.apprisk",
+          size: 5,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching droppedTrafficSeverityGovNet:", error);
+    throw new Error(
+      `Failed to fetch droppedTrafficSeverityGovNet data: ${error.message}`
+    );
+  }
+};
+// droppedTrafficSeverityInternet
+export const getDroppedTrafficSeverityInternet = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must_not: [{ prefix: { "data.srcip": "10." } }],
+        filter: { range: { "@timestamp": timeRange } },
+      },
+    },
+    aggs: {
+      top_attacks: {
+        terms: {
+          field: "data.apprisk",
+          size: 5,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching droppedTrafficSeverityInternet:", error);
+    throw new Error(
+      `Failed to fetch droppedTrafficSeverityInternet: ${error.message}`
+    );
+  }
+};
+// top5CountryTrafficAllowed
+export const getTop5CountryTrafficAllowed = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          { match: { "data.action": "pass" } },
+          { match: { "decoder.name": "fortigate-firewall-v5" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+    aggs: {
+      top_countries: {
+        terms: {
+          field: "data.srccountry",
+          size: 5,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching top5CountryTrafficAllowed:", error);
+    throw new Error(
+      `Failed to fetch top5CountryTrafficAllowed data: ${error.message}`
+    );
+  }
+};
+// top5CountryTrafficBlocked
+export const getTop5CountryTrafficBlocked = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          { match: { "data.action": "dropped" } },
+          { match: { "decoder.name": "fortigate-firewall-v5" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+    aggs: {
+      top_countries: {
+        terms: {
+          field: "data.srccountry",
+          size: 5,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching top5CountryTrafficBlocked:", error);
+    throw new Error(
+      `Failed to fetch top5CountryTrafficBlocked data: ${error.message}`
+    );
+  }
+};
+// vpnUsersConnected
+export const getVpnUsersConnected = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          { match: { "data.subtype": "vpn" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+    aggs: {
+      top_users: {
+        terms: {
+          field: "data.dstuser",
+          size: 5,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching getVpnUsersConnected:", error);
+    throw new Error(
+      `Failed to fetch getVpnUsersConnected data: ${error.message}`
+    );
+  }
+};
+// top10AppsUsedInternally
+export const getTop10AppsUsedInternally = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          { match: { "decoder.name": "cisco-ftd" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+    aggs: {
+      top_websites: {
+        terms: {
+          field: "data.WebApplication",
+          size: 10,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching top10AppsUsedInternally:", error);
+    throw new Error(
+      `Failed to fetch top10AppsUsedInternally data: ${error.message}`
+    );
+  }
+};
+// top10RequestedAppsGovNet
+export const getTop10RequestedAppsGovNet = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must: [
+          { match: { "decoder.name": "Barracuda_custom" } },
+          { range: { "@timestamp": timeRange } },
+        ],
+      },
+    },
+    aggs: {
+      top_websites: {
+        terms: {
+          field: "data.dhost",
+          size: 10,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching top10RequestedAppsGovNet:", error);
+    throw new Error(
+      `Failed to fetch top10RequestedAppsGovNet data: ${error.message}`
+    );
+  }
+};
+// top10RequestedAppsInternet
+export const getTop10RequestedAppsInternet = async (timeRange = {}) => {
+  const url = "/raven*/_search?pretty";
+  const data = {
+    size: 0,
+    query: {
+      bool: {
+        must_not: [{ prefix: { "data.srcip": "10." } }],
+        filter: { range: { "@timestamp": timeRange } },
+        must: [{ match: { "decoder.name": "Barracuda_custom" } }],
+      },
+    },
+    aggs: {
+      top_website: {
+        terms: {
+          field: "data.dhost",
+          size: 10,
+        },
+      },
+    },
+  };
+  try {
+    const response = await api.post(url, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching top10RequestedAppsInternet:", error);
+    throw new Error(
+      `Failed to fetch top10RequestedAppsInternet data: ${error.message}`
+    );
   }
 };
 
-  
-  
-  // Add more functions for other API requests...
-  
-  export default api;
+// Add more functions for other API requests...
+
+export default api;
